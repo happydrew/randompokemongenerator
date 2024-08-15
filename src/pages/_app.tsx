23f6eb1ec7b6012a8301e2453163f45cd04d0f30
zhugechina <zhugetd@126.com>
2024-08-15 19:49:32 +0800

    set google ads

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -46,6 +46,7 @@ export default function App({ Component, pageProps }: AppProps) {
       <Head>
         <link rel="icon" href="/favicon.ico" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4907197973761221" crossOrigin="anonymous"></script>
       </Head>
       {/* Google Tag Manager */}
       <Script
