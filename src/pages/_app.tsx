348396cebd6bcfc692ba8cddf4d474de7d3c475c
zhugechina <zhugetd@126.com>
2025-01-26 15:02:34 +0800

    add monetag ads

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -48,6 +48,9 @@ export default function App({ Component, pageProps }: AppProps) {
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4907197973761221" crossOrigin="anonymous"></script>
         <meta name="google-adsense-account" content="ca-pub-4907197973761221" />
+
+        {/* monetag ads - Native Banner (Interstitial) 8833465 */}
+        <script dangerouslySetInnerHTML={{ __html: `(function(d,z,s){s.src='https://'+d+'/401/'+z;try{(document.body||document.documentElement).appendChild(s)}catch(e){}})('groleegni.net',8833465,document.createElement('script'))` }}></script>
       </Head>
 
       {/* Google Tag Manager */}
