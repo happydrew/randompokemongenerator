46417868551525caa30e431c5703aeb23a6cc9ce
zhugechina <zhugetd@126.com>
2025-08-24 20:46:34 +0800

    optimize image load

--- src/components/generator-tool/generator-tool.tsx
+++ src/components/generator-tool/generator-tool.tsx
@@ -20,7 +20,7 @@ import {
 } from './display-options';
 
 import Link from 'next/link';
-import Image from 'next/image';
+import ExportedImage from 'next-image-export-optimizer';
 
 const handleToggleShinyClick = () => {
     toggleShinyDisplay();
@@ -101,7 +101,7 @@ const GeneratorTool: React.FC<GeneratorToolProps> = (props) => {
             <a className='w-full px-4 py-4 text-xl mb-4 flex flex-col md:flex-row justify-center gap-2 items-center bg-emerald-100'
                 href="https://compareheights.org/"
                 target='_blank'>
-                <Image src="https://compareheights.org/og-image.png"
+                <ExportedImage src="https://compareheights.org/og-image.png"
                     alt="Best Height Comparison Tool" width={256} height={128} className="inline-block mr-2 shadow-xl" />
                 <span className='leading-6 md:leading-10 text-base md:text-lg text-center items-center text-amber-600'>Curious how you measure up to celebrities, animals, or historical giants?<br />
                     CompareHeights lets you visualize it all in one click 📏✨</span>
