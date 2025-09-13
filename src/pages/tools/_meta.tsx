bbbda788c39cbffd9d80651ce3b5986d81745b53
zhugechina <zhugetd@126.com>
2025-09-13 17:13:49 +0800

    update

---
+++ src/pages/tools/_meta.tsx
@@ -0,0 +1,142 @@
+export default {
+  "random-pokemon-type-generator": {
+    type: "page",
+    title: "Random Pokemon Type Generator",
+    layout: "raw"
+  },
+  "random-shiny-pokemon-generator": {
+    type: "page",
+    title: "Random Shiny Pokemon Generator",
+    layout: "raw"
+  },
+  "random-mega-pokemon-generator": {
+    type: "page",
+    title: "Random Mega Pokemon Generator",
+    layout: "raw"
+  },
+  "random-pokemon-team-generator": {
+    type: "page",
+    title: "Random Pokemon Team Generator",
+    layout: "raw"
+  },
+  "aleatory-pokemon": {
+    type: "page",
+    title: "Aleatory Pokemon",
+    layout: "raw"
+  },
+  "give-me-a-random-pokemon": {
+    type: "page",
+    title: "Give Me A Random Pokemon",
+    layout: "raw"
+  },
+  "a-random-pokemon": {
+    type: "page",
+    title: "A Random Pokemon",
+    layout: "raw"
+  },
+  "2-random-pokemon": {
+    type: "page",
+    title: "2 Random Pokemon",
+    layout: "raw"
+  },
+  "3-random-pokemon": {
+    type: "page",
+    title: "3 Random Pokemon",
+    layout: "raw"
+  },
+  "random-crystal-pokemon-generator": {
+    type: "page",
+    title: "Random Crystal Pokemon Generator",
+    layout: "raw"
+  },
+  "random-emerald-pokemon-generator": {
+    type: "page",
+    title: "Random Emerald Pokemon Generator",
+    layout: "raw"
+  },
+  "random-firered-pokemon-generator": {
+    type: "page",
+    title: "Random FireRed Pokemon Generator",
+    layout: "raw"
+  },
+  "random-platinum-pokemon-generator": {
+    type: "page",
+    title: "Random Platinum Pokemon Generator",
+    layout: "raw"
+  },
+  "random-soulsilver-pokemon-generator": {
+    type: "page",
+    title: "Random SoulSilver Pokemon Generator",
+    layout: "raw"
+  },
+  "random-legendary-pokemon-generator": {
+    type: "page",
+    title: "Random Legendary Pokemon Generator",
+    layout: "raw"
+  },
+  "random-mythical-pokemon-generator": {
+    type: "page",
+    title: "Random Mythical Pokemon Generator",
+    layout: "raw"
+  },
+  "random-paradox-pokemon-generator": {
+    type: "page",
+    title: "Random Paradox Pokemon Generator",
+    layout: "raw"
+  },
+  "random-pokemon-generator-gen-1": {
+    type: "page",
+    title: "Random Pokemon Generator Gen 1",
+    layout: "raw"
+  },
+  "random-pokemon-generator-gen-2": {
+    type: "page",
+    title: "Random Pokemon Generator Gen 2",
+    layout: "raw"
+  },
+  "random-pokemon-generator-gen-3": {
+    type: "page",
+    title: "Random Pokemon Generator Gen 3",
+    layout: "raw"
+  },
+  "random-pokemon-generator-gen-4": {
+    type: "page",
+    title: "Random Pokemon Generator Gen 4",
+    layout: "raw"
+  },
+  "random-pokemon-generator-gen-5": {
+    type: "page",
+    title: "Random Pokemon Generator Gen 5",
+    layout: "raw"
+  },
+  "random-pokemon-generator-gen-6": {
+    type: "page",
+    title: "Random Pokemon Generator Gen 6",
+    layout: "raw"
+  },
+  "random-pokemon-generator-gen-7": {
+    type: "page",
+    title: "Random Pokemon Generator Gen 7",
+    layout: "raw"
+  },
+  "random-pokemon-generator-gen-8": {
+    type: "page",
+    title: "Random Pokemon Generator Gen 8",
+    layout: "raw"
+  },
+  "random-pokemon-generator-gen-9": {
+    type: "page",
+    title: "Random Pokemon Generator Gen 9",
+    layout: "raw"
+  },
+  "random-pokemon-maker": {
+    type: "page",
+    title: "Random Pokemon Maker",
+    layout: "raw"
+  },
+  "random-pokemon-picker": {
+    type: "page",
+    title: "Random Pokemon Picker",
+    layout: "raw"
+  }
+}
