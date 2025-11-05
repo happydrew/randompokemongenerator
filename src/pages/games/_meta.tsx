47342b26a5987eefd51bbecf3bfc55afb7fa87c7
zhugechina <zhugetd@126.com>
2025-11-06 00:14:31 +0800

    update

--- src/pages/games/_meta.tsx
+++ src/pages/games/_meta.tsx
@@ -1,4 +1,11 @@
 export default {
+  "brother-hai-restaurant": {
+    type: "page",
+    title: "Brother Hai's Pho Restaurant",
+    theme: {
+      layout: "raw"
+    }
+  }
   // "backyard-baseball": {
   //   type: "page",
   //   title: "Backyard Baseball",
