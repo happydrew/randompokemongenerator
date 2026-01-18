292c2ccc148bcd27ee82782a12d4efe175166a38
zhugechina <zhugetd@126.com>
2026-01-18 17:23:58 +0800

    migrate to cloudflare

---
+++ src/pages/_error.tsx
@@ -0,0 +1,22 @@
+import { NextPageContext } from 'next';
+
+export const runtime = 'edge';
+
+function Error({ statusCode }: { statusCode?: number }) {
+  return (
+    <div className="flex h-dvh items-center justify-center flex-col">
+      <h1 className="text-white text-4xl">
+        {statusCode
+          ? `An error ${statusCode} occurred on server`
+          : 'An error occurred on client'}
+      </h1>
+    </div>
+  );
+}
+
+Error.getInitialProps = ({ res, err }: NextPageContext) => {
+  const statusCode = res ? res.statusCode : err ? err.statusCode : 404;
+  return { statusCode };
+};
+
+export default Error;
