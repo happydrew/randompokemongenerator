9c88220c8ddd83fb47a302896f681261140b8e1e
zhugechina <zhugetd@126.com>
2026-01-18 16:50:39 +0800

    migrate to cloudflare

--- next.config.js
+++ next.config.js
@@ -37,13 +37,13 @@ export default withLess(
       })
       return config
     },
-    output: "export",
+    // output: "export",
     images: {
       // loader: "custom",
       // imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
       // deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
       domains: ['compareheights.org'],
-      unoptimized: true
+      // unoptimized: true
     },
     transpilePackages: ["next-image-export-optimizer"],
     env: {
